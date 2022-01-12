e31c154b274a0dae4aca57435d306434ab5252d2
Vitalik <dazorchik@gmail.com>
2022-01-12 17:20:16 +0200

    lesson

--- src/public/index.php
+++ src/public/index.php
@@ -12,60 +12,13 @@
 </head>
 
 <body style="margin: 0;">
-<?php
-$name = "oleg";
-echo 'my name ', $name, "<br>";
-$a = 8;
-$b = 1;
-echo "сумма ", $a + $b, "<br>";
-echo "разность ", $a - $b, "<br>";
-echo "произведение ", $a * $b, "<br>";
-echo "деление ", $a / $b, "<br>";
-echo "остаток от деление ", $a % $b, "<br>";
-$number = 5;
-$result = pow($number, 3);
-echo $number, ' в степени 3 = ', $result, "<br>";
-$age = 28;
-if (($age >= 18) && ($age <= 60)) {
-    echo "Вам ещё работать и работать", "<br>";
-}
-if ($age < 18) {
-    echo "Вам ещё рано работать", "<br>";
-}
-if ($age > 60) {
-    echo "Пора на отдых", "<br>";
-}
-$dayNumber = 5;
-if ($dayNumber <= 7) {
-    if ($dayNumber <= 5) {
-        echo "Это рабочий день", "<br>";
-    }
-    if ($dayNumber > 5) {
-        echo "Это выходной", "<br>";
-    }
-} else {
-    echo "Ошибка, не правильный день недели", "<br>";
-}
-define("DAYS_COUNT", 7);
-const MONTH_COUNT = 12;
-echo "Количество дней в неделе ", DAYS_COUNT, " Количество месяцев в году ", MONTH_COUNT, "<br>";
-$first = 4;
-$second = 3;
-if ($first == $second) {
-    echo "Сумма чисел равна ", $first + $second, "<br>";
-}
-if ($first !== $second) {
-    echo "Разница чисел равна ", $first - $second, "<br>";
-}
-$random = rand(1, 100);
-if ($random % 2 == 0) {
-    echo "Число $random  кратно 2";
-} else {
-    echo "Число $random  не кратно 2";
-}
+<form id='forma' action='login.php' method='post'>
+    <h1>Форма входа</h1>
+    <p>Заполните поля для входа на сайт</p>
+    <p>Логин<br /><input type='text' name='login'value="<?php if(isset($_COOKIE['login'])){echo $_COOKIE['login'];} else {echo "";} ?>"></p>
+    <p>Пароль<br /><input type='password' name='password' value="<?php if(isset($_COOKIE['password'])){echo $_COOKIE['password'];} else {echo "";} ?>"></p>
+    <p><input type='submit' name='submit' value='Войти'> <br></p></form>"
 
-
-?>
 
 </body>
 
