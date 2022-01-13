c0a2083fa1788e734c74d837a727cc516669c3a9
Vitalik <dazorchik@gmail.com>
2022-01-13 22:19:13 +0200

    lesson 4

--- src/public/index.php
+++ src/public/index.php
@@ -12,13 +12,89 @@
 </head>
 
 <body style="margin: 0;">
-<form id='forma' action='login.php' method='post'>
-    <h1>Форма входа</h1>
-    <p>Заполните поля для входа на сайт</p>
-    <p>Логин<br /><input type='text' name='login'value="<?php if(isset($_COOKIE['login'])){echo $_COOKIE['login'];} else {echo "";} ?>"></p>
-    <p>Пароль<br /><input type='password' name='password' value="<?php if(isset($_COOKIE['password'])){echo $_COOKIE['password'];} else {echo "";} ?>"></p>
-    <p><input type='submit' name='submit' value='Войти'> <br></p></form>"
+<?php
+$file1 = fopen('text.txt', 'w+');
+fwrite($file1, "hello palmo");
+$text = file_get_contents('text.txt');
+echo $text;
+$fileInfo = stat('text.txt');
+echo "<br>", "Размер в байтах ", $fileInfo["size"], "<br>";
+echo "<br>", "Размер в мегабайтах ", $fileInfo["size"] / (1024 * 1024), "<br>";
+echo "<br>", "Размер в гигабайтах ", $fileInfo["size"] / (1024 * 1024 * 1024), "<br>";
+fclose($file1);
+$file2 = fopen('text2.txt', 'w');
+unlink("text2.txt");
+if (!is_dir("testDir")) {
+    mkdir("testDir", 0777);
+}
+$arrDir = ["test1", "test2", "test3"];
+foreach ($arrDir as $dirName) {
+    if (!is_dir("testDir/$dirName")) {
+        mkdir("testDir/$dirName", 0777);
+    }
 
+    $file1 = fopen("testDir/$dirName/text.txt", 'w+');
+    fwrite($file1, "hello palmo");
+    $text = file_get_contents("testDir/$dirName/text.txt");
+    echo $text, "<br>";
+
+}
+function fileAsDir($str)
+{
+    if (is_dir($str)) {
+        return "dir";
+    } else if (file_exists($str)) {
+        return "file";
+    } else {
+        return false;
+    }
+
+}
+
+echo "<br>", fileAsDir("text.txt");
+
+function sizeDir($dir)
+{
+    $size = 0;
+    $arrDir = scandir($dir);
+    foreach ($arrDir as $name) {
+        if (is_file($name)) {
+            $fileInfo = stat("$name");
+            $size = $size + (int)$fileInfo["size"];
+        } else if (is_dir($name)) {
+            $arrDir2 = scandir($name);
+            foreach ($arrDir2 as $name2) {
+                if (is_file($name2)) {
+                    $fileInfo = stat("$name2");
+                    $size = $size + (int)$fileInfo["size"];
+                }
+            }
+
+        }
+
+    }
+    return $size;
+}
+
+echo sizeDir("testDir");
+
+function deletDir($dir){
+    $arrDir = scandir($dir);
+    foreach ($arrDir as $name) {
+        if(($name != '.') && ($name != '..')) {
+            if (is_file("$dir/$name")) {
+                unlink("$dir/$name");
+            } else if (is_dir("$dir/$name")) {
+                deletDir("$dir/$name");
+            }
+
+            rmdir("$dir");
+        }
+    }
+}
+
+deletDir("testDir");
+?>
 
 </body>
 
