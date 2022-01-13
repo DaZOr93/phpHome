8573290dc632b5c8d5ab11a29673ffbdc9c2b49e
Vitalik <dazorchik@gmail.com>
2022-01-14 00:48:38 +0200

    DZ5

--- src/public/index.php
+++ src/public/index.php
@@ -13,89 +13,40 @@
 
 <body style="margin: 0;">
 <?php
-$file1 = fopen('text.txt', 'w+');
-fwrite($file1, "hello palmo");
-$text = file_get_contents('text.txt');
-echo $text;
-$fileInfo = stat('text.txt');
-echo "<br>", "Размер в байтах ", $fileInfo["size"], "<br>";
-echo "<br>", "Размер в мегабайтах ", $fileInfo["size"] / (1024 * 1024), "<br>";
-echo "<br>", "Размер в гигабайтах ", $fileInfo["size"] / (1024 * 1024 * 1024), "<br>";
-fclose($file1);
-$file2 = fopen('text2.txt', 'w');
-unlink("text2.txt");
-if (!is_dir("testDir")) {
-    mkdir("testDir", 0777);
-}
-$arrDir = ["test1", "test2", "test3"];
-foreach ($arrDir as $dirName) {
-    if (!is_dir("testDir/$dirName")) {
-        mkdir("testDir/$dirName", 0777);
-    }
+$fruits = fopen('fruits.txt', 'a+');
+fwrite($fruits, "Яблоко".PHP_EOL);
+fwrite($fruits, "Груша".PHP_EOL);
+fwrite($fruits, "Банан".PHP_EOL);
+fwrite($fruits, "Слива".PHP_EOL);
+fwrite($fruits, "Вишня".PHP_EOL);
+fwrite($fruits, "Черешня".PHP_EOL);
+fwrite($fruits, "Персик".PHP_EOL);
+fwrite($fruits, "Смородина".PHP_EOL);
+fwrite($fruits, "Малина".PHP_EOL);
+fwrite($fruits, "Ананас".PHP_EOL);
+$arrFruits = file("fruits.txt");
+sort($arrFruits);
+file_put_contents('fruits.txt', $arrFruits);
 
-    $file1 = fopen("testDir/$dirName/text.txt", 'w+');
-    fwrite($file1, "hello palmo");
-    $text = file_get_contents("testDir/$dirName/text.txt");
-    echo $text, "<br>";
-
-}
-function fileAsDir($str)
-{
-    if (is_dir($str)) {
-        return "dir";
-    } else if (file_exists($str)) {
-        return "file";
-    } else {
-        return false;
-    }
-
-}
-
-echo "<br>", fileAsDir("text.txt");
-
-function sizeDir($dir)
-{
-    $size = 0;
+function deleteBigFiles($dir){
     $arrDir = scandir($dir);
-    foreach ($arrDir as $name) {
-        if (is_file($name)) {
-            $fileInfo = stat("$name");
-            $size = $size + (int)$fileInfo["size"];
-        } else if (is_dir($name)) {
-            $arrDir2 = scandir($name);
-            foreach ($arrDir2 as $name2) {
-                if (is_file($name2)) {
-                    $fileInfo = stat("$name2");
-                    $size = $size + (int)$fileInfo["size"];
-                }
+    foreach ($arrDir as $name){
+        if (is_file("$dir/$name")){
+            $fileSize = stat("$dir/$name")["size"]/(1024*1024);
+            if($fileSize >= 1){
+               unlink("$dir/$name");
             }
-
-        }
-
-    }
-    return $size;
-}
-
-echo sizeDir("testDir");
-
-function deletDir($dir){
-    $arrDir = scandir($dir);
-    foreach ($arrDir as $name) {
-        if(($name != '.') && ($name != '..')) {
-            if (is_file("$dir/$name")) {
-                unlink("$dir/$name");
-            } else if (is_dir("$dir/$name")) {
-                deletDir("$dir/$name");
-            }
-
-            rmdir("$dir");
         }
     }
 }
-
-deletDir("testDir");
+deleteBigFiles("uploads");
 ?>
+<form action="file.php" method="post" enctype="multipart/form-data" >
+    <p>Выберите файл для отображения на другой странице</p>
+    <label for="file"><input type="file" name="textFile"></label>
 
+    <input type="submit">
+</form>
 </body>
 
 </html>
