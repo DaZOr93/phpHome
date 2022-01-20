c8201ef17d88a530a616d15ae8177580ad15d3c7
Vitalik <dazorchik@gmail.com>
2022-01-20 22:48:01 +0200

    DZ6

--- src/public/home.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if ($_SESSION["authorization"] == 'authorization'){
-    echo "Добро пожаловать ". $_SESSION["login"];
-    echo "<a href='logout.php'> <button>Выйти</button></a>";
-}
--- src/public/index.php
+++ src/public/index.php
@@ -1,16 +1,97 @@
 <?php
-session_start();
-if (isset($_SESSION["authorization"])) {
-    if ($_SESSION["authorization"] == 'authorization') {
-        echo "Вы авторизованы ","<a href=home.php>Перейти на домашнюю страницу</a>";
-    }
-} else {
-    echo "<a href=reg.php>
-    <button>Регистрация</button>
-</a>
 
-<a href=authorization.php>
-    <button>Авторизация</button>
-</a>";
+class Figure
+{
+    protected $type;
+
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getSquare()
+    {
+
+    }
+
+    public function getPerimeter()
+    {
+
+    }
+
 }
+
+class Circle extends Figure
+{
+    public function __construct($radius)
+    {
+        $this->type = "Круг";
+        $this->radius = $radius;
+
+    }
+
+    public function getSquare()
+    {
+        return 3.14 * $this->radius ** 2;
+    }
+
+    public function getPerimeter()
+    {
+        return 3.14 * $this->radius * 2;
+    }
+}
+
+$circle = new Circle(5);
+echo "Фигура: ", $circle->getType(), ", площадь: ", $circle->getSquare(), " периметр: ", $circle->getPerimeter(),"<br>";
+
+class Triangle extends Figure
+{
+    public function __construct($a, $b, $c)
+    {
+        $this->type = "Треугольник";
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }
+
+    public function getSquare()
+    {
+
+        $p = $this->getPerimeter() / 2;
+        return sqrt($p * ($p - $this->a) * ($p - $this->b) * ($p - $this->c));
+    }
+
+    public function getPerimeter()
+    {
+        return $this->a + $this->b + $this->c;
+    }
+}
+$triangle = new Triangle(5,3,4);
+echo "Фигура: ", $triangle->getType(), ", площадь: ", $triangle->getSquare(), " периметр: ", $triangle->getPerimeter(),"<br>";
+
+
+class Square extends Figure
+{
+    public function __construct($a, $b)
+    {
+        $this->type = "Квадрат";
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function getSquare()
+    {
+
+        return $this->a * $this->b;
+    }
+
+    public function getPerimeter()
+    {
+        return ($this->a + $this->b)*2;
+    }
+}
+$square = new Square(5,3);
+echo "Фигура: ", $square->getType(), ", площадь: ", $square->getSquare(), " периметр: ", $square->getPerimeter(),"<br>";
+
 ?>
