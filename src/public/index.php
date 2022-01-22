02362ff54beb6be9537af11101e69f3ac10b3349
Vitalik <dazorchik@gmail.com>
2022-01-22 18:03:45 +0200

    done

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<button><a href='newsAdd.php'>Список новостей</a></button>";
+echo "<button><a href='newsAdd.php'>Добавить новость</a></button>";
 echo "<button><a href='newsList.php'>Список новостей</a></button>";
 ?>
 
--- src/public/newsAdd.php
+++ src/public/newsAdd.php
@@ -1,107 +1,104 @@
 <?php
-if (empty($_COOKIE["newsPublished"])){
+if (empty($_COOKIE["newsPublished"])) {
     $newsPublished = [];
-    setcookie("newsPublished",json_encode($newsPublished), time()+3600);
+    setcookie("newsPublished", json_encode($newsPublished), time() + 3600);
 }
-function notClear($data){
-    if (trim($data) !== ''){
-    return true;
+function notClear($data)
+{
+    if (trim($data) !== '') {
+        return true;
     }
     return false;
 }
-function minCharacters($data, $characters){
-    if ( mb_strlen($data) < $characters){
+
+function minCharacters($data, $characters)
+{
+    if (mb_strlen($data) < $characters) {
         return false;
-    } else{
+    } else {
         return true;
     }
 
 }
 
-
-if (!empty($_POST)){
+if (!empty($_POST)) {
     $error = 0;
-    if (notClear($_POST["nameNews"])){
+    if (notClear($_POST["nameNews"])) {
         $nameNews = $_POST["nameNews"];
 
-    } else{
+    } else {
         $error = 1;
     }
-    if (notClear($_POST["descriptionNews"])){
-        if (minCharacters($_POST["descriptionNews"],5)){
+    if (notClear($_POST["descriptionNews"])) {
+        if (minCharacters($_POST["descriptionNews"], 20)) {
             $descriptionNews = $_POST["descriptionNews"];
-        } else{
+        } else {
             $error = 1;
         }
-    } else{
+    } else {
         $error = 1;
     }
-    if (notClear($_POST["dataNews"])){
-        $dataNews =  $_POST["dataNews"];
+    if (notClear($_POST["dataNews"])) {
+        $dataNews = $_POST["dataNews"];
     } else {
         $dataNews = date('d-m-Y');
     }
-    if (notClear($_POST["textNews"])){
-        if (minCharacters($_POST["textNews"], 5)){
+    if (notClear($_POST["textNews"])) {
+        if (minCharacters($_POST["textNews"], 100)) {
             $textNews = $_POST["textNews"];
         } else {
             $error = 1;
         }
-    } else{
-            $error = 1;
-        }
-    if (notClear($_POST["authorNews"])){
+    } else {
+        $error = 1;
+    }
+    if (notClear($_POST["authorNews"])) {
         $authorNews = $_POST["authorNews"];
 
-    } else{
+    } else {
         $error = 1;
     }
-    if (isset($_FILES["imageNews"])){
+    if (isset($_FILES["imageNews"])) {
 
-        if(explode('/', $_FILES['imageNews']['type'])[0] === 'image') {
+        if (explode('/', $_FILES['imageNews']['type'])[0] === 'image') {
 
-            $current_patch  = $_FILES['imageNews']['tmp_name'];
+            $current_patch = $_FILES['imageNews']['tmp_name'];
             $imageNews = $_FILES["imageNews"];
             $imageNewsName = $_FILES["imageNews"]["name"];
-            $newPatch = dirname(__FILE__).'/uploads/'. $imageNewsName;
-            $filePatch = 'uploads/'. $imageNewsName;
+            $newPatch = dirname(__FILE__) . '/uploads/' . $imageNewsName;
+            $filePatch = 'uploads/' . $imageNewsName;
             move_uploaded_file($current_patch, $newPatch);
-        } else{
+        } else {
             $error = 1;
         }
 
-    } else{
+    } else {
         $error = 1;
     }
 
 
+    if (notClear($_POST["statusNews"])) {
+        if (($_POST["statusNews"] == 'published') && ($error !== 1)) {
 
-    if (notClear($_POST["statusNews"])){
-        if (($_POST["statusNews"] == 'published') && ($error !== 1)){
-
-            $newsPublished = json_decode($_COOKIE["newsPublished"],true);
-            $news = ["nameNews" => $nameNews, "descriptionNews"=>$descriptionNews, "dataNews"=>$dataNews, "textNews"=>$textNews, "authorNews"=>$authorNews, "imageNews"=>$filePatch];
+            $newsPublished = json_decode($_COOKIE["newsPublished"]);
+            $news = ["nameNews" => $nameNews, "descriptionNews" => $descriptionNews, "dataNews" => $dataNews, "textNews" => $textNews, "authorNews" => $authorNews, "imageNews" => $filePatch];
             $newsPublished[] = $news;
             $newsPublished = json_encode($newsPublished);
-            setcookie("newsPublished",$newsPublished);
+            setcookie("newsPublished", $newsPublished);
+            echo "Новость добавлена";
+            echo "<button><a href='newsList.php'>Список новостей</a></button>";
 
         }
     }
 
 
-
-
-
-
-
-
-    if ($error == 1){
+    if ($error == 1) {
         echo "Ошибка";
     }
 }
 
 ?>
-<form action="newsAdd.php"  method="post" enctype="multipart/form-data" >
+<form action="newsAdd.php" method="post" enctype="multipart/form-data">
     <label for="nameNews"><b>Имя новости</b></label>
     <input type="text" placeholder="Введите Название новости" name="nameNews">
     <br>
@@ -112,13 +109,13 @@ if (!empty($_POST)){
     <input type="date" name="dataNews">
     <br>
     <label for="statusNews"><b>Статус</b></label>
-    <input type="radio"  value="draft" name="statusNews"  checked>
+    <input type="radio" value="draft" name="statusNews" checked>
     <label for="statusNews"><b>черновик</b></label>
-    <input type="radio"  value="published" name="statusNews">
+    <input type="radio" value="published" name="statusNews">
     <label for="statusNews"><b>опубликовано</b></label>
     <br>
     <label for="textNews""><b>текст новости</b></label>
-    <textarea rows="10" cols="45" name="textNews" placeholder="текст новости"></textarea>
+    <textarea rows="3" cols="45" name="textNews" placeholder="текст новости"></textarea>
     <br>
     <label for="authorNews"><b>автор новости</b></label>
     <input type="text" placeholder="Введите автора новости" name="authorNews">
